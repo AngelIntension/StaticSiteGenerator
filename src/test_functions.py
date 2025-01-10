cf2f4e9b77d98c7567b01bba605fa444aeeec4db
AngelIntension <orphic.singularity@protonmail.com>
2025-01-10 12:31:30 -0600

    add functions to extract image and link fields from markdown

--- src/test_functions.py
+++ src/test_functions.py
@@ -1,9 +1,9 @@
 import unittest
 from textnode import TextNode, TextType
-from functions import split_nodes_delimiter
+from functions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
-class TestFunctions(unittest.TestCase):
-    def test_split_nodes_delimiter_should_split_single_node(self):
+class SplitNodesDelimiterShould(unittest.TestCase):
+    def test_split_single_node(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         actual = split_nodes_delimiter([node], "`", TextType.CODE)
         expected = [
@@ -13,7 +13,7 @@ class TestFunctions(unittest.TestCase):
         ]
         self.assertEqual(actual, expected)
 
-    def test_split_nodes_delimiter_should_split_multiple_nodes(self):
+    def test_split_multiple_nodes(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         node2 = TextNode("This is text with a **bold** word", TextType.TEXT)
         actual = split_nodes_delimiter([node, node2], "**", TextType.BOLD)
@@ -25,7 +25,7 @@ class TestFunctions(unittest.TestCase):
         ]
         self.assertEqual(actual, expected)
 
-    def test_split_nodes_delimiter_should_split_node_with_multiple_delimited_strings(self):
+    def test_split_node_with_multiple_delimited_strings(self):
         node = TextNode("This is text with a `code block` and another `other code block` phrase", TextType.TEXT)
         actual = split_nodes_delimiter([node], "`", TextType.CODE)
         expected = [
@@ -37,7 +37,7 @@ class TestFunctions(unittest.TestCase):
         ]
         self.assertEqual(actual, expected)
 
-    def test_split_nodes_delimiter_should_split_node_starting_with_delimiter(self):
+    def test_split_node_starting_with_delimiter(self):
         node = TextNode("`code block` with additional text", TextType.TEXT)
         actual = split_nodes_delimiter([node], "`", TextType.CODE)
         expected = [
@@ -46,7 +46,7 @@ class TestFunctions(unittest.TestCase):
         ]
         self.assertEqual(actual, expected)
 
-    def test_split_nodes_delimiter_should_return_non_text_nodes_without_changes(self):
+    def test_return_non_text_nodes_without_changes(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         node2 = TextNode("This is a bold node", TextType.BOLD)
         actual = split_nodes_delimiter([node, node2], "`", TextType.CODE)
@@ -58,8 +58,38 @@ class TestFunctions(unittest.TestCase):
         ]
         self.assertEqual(actual, expected)
 
-    def test_split_nodes_delimiter_should_raise_exception_if_no_closing_delimiter(self):
+    def test_raise_exception_if_no_closing_delimiter(self):
         node = TextNode("This is text with a `code block word", TextType.TEXT)
         with self.assertRaises(Exception) as cm:
             split_nodes_delimiter([node], "`", TextType.CODE)
         self.assertEqual(str(cm.exception), "text contains unmatched delimiter: '`'")
+
+class ExtractMarkdownImagesShould(unittest.TestCase):
+    def test_extract_valid_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        actual = extract_markdown_images(text)
+        expected = [
+            ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+            ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
+        ]
+        self.assertEqual(actual, expected)
+
+    def test_not_extract_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        actual = extract_markdown_images(text)
+        self.assertEqual(actual, [])
+
+class ExtractMarkdownLinksShould(unittest.TestCase):
+    def test_extract_valid_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        actual = extract_markdown_links(text)
+        expected = [
+            ("to boot dev", "https://www.boot.dev"),
+            ("to youtube", "https://www.youtube.com/@bootdotdev")
+        ]
+        self.assertEqual(actual, expected)
+
+    def test_not_extract_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        actual = extract_markdown_links(text)
+        self.assertEqual(actual, [])
